cd5a0f5084c0abbdbe4c72275e5e620aa5572cf9
chltrn <chltrn@users.noreply.github.com>
2021-01-06 16:12:07 -0500

    add more color themes and mp3 choices

--- script.js
+++ script.js
@@ -19,55 +19,151 @@ const colorThemeThree = ["#75EBD6","#75E9D5","#77E8D6","#7AE6D7","#7BE5D7",
     "#9AC7E0","#9BC6E0","#9DC5E1","#9EC3E1","#9FC1E1","#A0C2E2","#A3C0E3",
     "#A4BEE3","#A6BCE3","#A7BCE4","#A8BAE4","#AAB9E5","#ABB7E4","#ACB6E5"];
 
+const songUrls = ['https://res.cloudinary.com/duvpi1rvn/video/upload/v1609947969/Ikson_-_Last_Summer_xyn0go.mp3',
+'https://res.cloudinary.com/duvpi1rvn/video/upload/v1609954446/Jaylon_Ashaun_-_Goosebumps_zv7t66.mp3'];
+
 const numBars = 40;
 const container = document.querySelector(".container");
-const audio = document.querySelector("audio");
-const index = 25;   // determines frequency index interval
+let audio = document.querySelector("audio");
+const playBtn = document.getElementById("playbtn");
+const pauseBtn = document.getElementById("pausebtn");
+const colors = document.querySelectorAll(".color");
+const songs = document.querySelectorAll(".mp3");
+
+const index = 23;   // determines frequency index interval
 const offset = 8;   // determines what frequency to start indexing at
 let firstPlay = 0 ;
 let audioInfo;
+const defaultBarHeight = 13; 
 
 class AudioInfo {
-  constructor(analyzerNode,freqDataArray){
+  constructor(audioCtx,analyzerNode,freqDataArray){
+      this.audioCtx = audioCtx;
       this.analyzerNode = analyzerNode;
       this.freqDataArray = freqDataArray;
   }
 }
 
-function play() {  // make custom play button and transfer this functionality onto it
+// change height of bars according to decibal value stored in frequency data array
+function displayBars(){
+
+    let analyzerNode = audioInfo.analyzerNode;
+    let freqDataArray = audioInfo.freqDataArray;
+  
+    analyzerNode.getByteFrequencyData(freqDataArray); // get frequencies present at current audio moment
+  
+    for(let i = 0; i < numBars ; i++) { // loop through bars and change their heights
+  
+      base = freqDataArray[( i * index ) + offset ];             // get frequencies
+      backOne = freqDataArray[( ( i * index ) + offset ) - 1];
+      forwardOne  = freqDataArray[( ( i * index ) + offset ) + 1];
+  
+      let averagedFreq = ( backOne + base + forwardOne ) / 3;    // average them
+      let bar = document.querySelector("#bar" + i);              // get current bar associated with frequency
+      let height = Math.max(averagedFreq, defaultBarHeight);                   // calculate bar height
+      bar.style.height = Math.round(height * 1.5) + "px";        // display bar
+    }
+    requestAnimationFrame(displayBars);
+  }
+  
+  // create audio bars for visualization
+  function createBars(){
+     
+      for(let i = 0; i < numBars; i++) {
+          let bar = document.createElement("div");
+          bar.setAttribute("id", "bar"+ i);
+          bar.setAttribute("class", "bar");
+          bar.style.background = colorThemeOne[i];
+          $(".container").append(bar);             // display bars on DOM
+      }
+  }
+  
+  function changeColor(colorTheme){
+      console.log(colorTheme);
+      let theme; 
+      if(colorTheme == 'citrus'){
+          theme = colorThemeOne; 
+      }else if(colorTheme == 'galaxy'){
+          theme = colorThemeTwo; 
+      }else{
+          theme = colorThemeThree;
+      }
+      for(let i=0; i< numBars; i++){
+          let bar = document.getElementById("bar"+i);
+          bar.style.background = theme[i];
+      }
+  }
+
+function createAudioContext(){
+    let AudioContex = window.AudioContext || window.webkitAudioContext;  // create new Audio Context object
+    let audioCtx = new AudioContext();
+    console.log(audioCtx.state);
+
+    let sourceNode = audioCtx.createMediaElementSource(audio); // pass DOM audio into Audio Context to create the source node
+
+    let analyzerNode = audioCtx.createAnalyser(); // create processing node
+    analyzerNode.fftSize = 2048;  // define fast fourier transform window size
+    analyzerNode.smoothingTimeConstant = 0.9;
+
+    sourceNode.connect(analyzerNode).connect(audioCtx.destination);  // connect input node to analyzer node and then to the destination node
+    console.log(audioCtx.state);
+
+    let freqArrayLength = analyzerNode.frequencyBinCount; // the number of frequency values we have available for visualization
+    let freqDataArray = new Uint8Array(freqArrayLength); // create frequency array of unsigned integer type and of corresponding frequency length
+
+    // pass the frequency data array into the analyzer node and change it into byte format
+    analyzerNode.getByteFrequencyData(freqDataArray);
+    audioInfo = new AudioInfo(audioCtx,analyzerNode,freqDataArray);
+}
+
+function changeSong(id){
+
+    audio.remove(); // delete html audio element 
+    audio = null;   // set global variable to empty 
+
+    audio = document.createElement("AUDIO"); // create new html audio element 
+    audio.crossOrigin = "anonymous"; 
+
+    if(id == 'song1'){  // identify new audio source
+        audio.src = songUrls[0];
+    }else{ 
+        audio.src = songUrls[1];
+    }
+
+    audioInfo.audioCtx.close().then(function() {  // remove current audio context
+        createAudioContext();  // create new audio context 
+    });
+
+    audio.play();  // play new audio 
+}
+
+colors.forEach(function(color) {
+    color.addEventListener('click', e => {
+        changeColor(color.innerHTML);
+    })
+})
+
+songs.forEach(function(song) {
+    song.addEventListener('click', e => {
+        
+        changeSong(song.id);
+    })
+})
+
+playBtn.addEventListener('click', e => { //listen for click
     if(firstPlay == 0 ){
-
-        const AudioContex = window.AudioContext || window.webkitAudioContext;  // create new Audio Context object
-        const audioCtx = new AudioContext();
-        console.log(audioCtx.state);
-
-        const sourceNode = audioCtx.createMediaElementSource(audio); // pass DOM audio into Audio Context to create the source node
-
-        const analyzerNode = audioCtx.createAnalyser(); // create processing node
-        analyzerNode.fftSize = 2048;  // define fast fourier transform window size
-        analyzerNode.smoothingTimeConstant = 0.9;
-
-        sourceNode.connect(analyzerNode).connect(audioCtx.destination);  // connect input node to analyzer node and then to the destination node
-        console.log(audioCtx.state);
-
-        const freqArrayLength = analyzerNode.frequencyBinCount; // the number of frequency values we have available for visualization
-        const freqDataArray = new Uint8Array(freqArrayLength); // create frequency array of unsigned integer type and of corresponding frequency length
-
-        // pass the frequency data array into the analyzer node and change it into byte format
-        analyzerNode.getByteFrequencyData(freqDataArray);
-        audioInfo = new AudioInfo(analyzerNode,freqDataArray);
-
-        audio.play();  // also move this functionality into custom play button
+        createAudioContext();
+        audio.play(); 
     }else{  // not the first click
         audio.play();
     }
     firstPlay++;
     console.log(audioInfo);
-}
+})
 
-function pause(){
+pauseBtn.addEventListener('click', e => {
     audio.pause();
-}
+})
 
 audio.addEventListener('play', function() {
     console.log("um");
@@ -76,40 +172,6 @@ audio.addEventListener('play', function() {
         displayBars();  // on audio play, call the animation
     }
 });
-
-
-// change height of bars according to decibal value stored in frequency data array
-function displayBars(){
-
-  let analyzerNode = audioInfo.analyzerNode;
-  let freqDataArray = audioInfo.freqDataArray;
-
-  analyzerNode.getByteFrequencyData(freqDataArray); // get frequencies present at current audio moment
-
-  for(let i = 0; i < numBars ; i++) { // loop through bars and change their heights
-
-    base = freqDataArray[( i * index ) + offset ];             // get frequencies
-    backOne = freqDataArray[( ( i * index ) + offset ) - 1];
-    forwardOne  = freqDataArray[( ( i * index ) + offset ) + 1];
-
-    let averagedFreq = ( backOne + base + forwardOne ) / 3;    // average them
-    let bar = document.querySelector("#bar" + i);              // get current bar associated with frequency
-    let height = Math.max(averagedFreq, 33);                   // calculate bar height
-    bar.style.height = Math.round(height * 1.5) + "px";        // display bar
-  }
-  requestAnimationFrame(displayBars);
-}
-
-// create audio bars for visualization
-function createBars(){
-    for(let i = 0; i < numBars ; i++) {
-        let bar = document.createElement("div");
-        bar.setAttribute("id", "bar"+ i);
-        bar.setAttribute("class", "bar");
-        bar.style.background = colorThemeOne[i];
-        $(".container").append(bar);             // display bars on DOM
-    }
-}
 
 $( document ).ready(function() {
     createBars();
